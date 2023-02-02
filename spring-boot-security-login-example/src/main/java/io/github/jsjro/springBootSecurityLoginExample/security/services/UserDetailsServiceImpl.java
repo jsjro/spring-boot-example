43241d4ed7916f97662425c4a919a7cc012b9b55
jsjro <racotim@gmail.com>
2023-02-02 10:27:02 +0900

    MOD modify spring-boot-security-login-example

---
+++ spring-boot-security-login-example/src/main/java/io/github/jsjro/springBootSecurityLoginExample/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package io.github.jsjro.springBootSecurityLoginExample.security.services;
+
+import io.github.jsjro.springBootSecurityLoginExample.model.User;
+import io.github.jsjro.springBootSecurityLoginExample.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
